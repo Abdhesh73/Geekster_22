3477821d065171d589de51f1b8c50acf7eaf1e63
Abdhesh <abdheshkushwaha9784@gmail.com>
2022-04-10 17:56:09 +0530

    assignment added

---
+++ Assignment/April/Week_1/Assignment_14/Assignment/javaScript.js
@@ -0,0 +1,18 @@
+
+
+const x= prompt("Enter First Number:");
+const y= prompt("Enter second Num: ");
+// const z= prompt("Enter third num: ");
+
+if(x<y){
+    
+    console.log("X is greater");
+
+
+}
+else{
+   console.log("y is greater");
+}
+
+
+ 
